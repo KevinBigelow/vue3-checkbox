141cbf565072780fce45f9fb13e041da2382a90d
Kevin Bigelow <kbigelow5@gmail.com>
2022-09-04 17:05:12 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="bg-gray-300 inset-0 absolute flex flex-column items-center justify-center"
+  >
+    <check-box fieldId="volume" v-model:checked="volumeOn" label="Volume On" />
+  </div>
+</template>
+
+<script>
+import Checkbox from "./components/checkbox.vue";
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const volumeOn = ref(true);
+    return {
+      volumeOn,
+    };
+  },
+  components: {
+    "check-box": Checkbox,
+  },
+};
+</script>
+
+<style>
+</style>
